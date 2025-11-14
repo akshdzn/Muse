7a12ed4cc116568d215ad9448b056451f6dc66ee
akshdzn <222266665+akshdzn@users.noreply.github.com>
2025-11-14 18:32:32 +0530

    added time display

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,47 @@
+<script>
+    let date = new Date();
+    let time = date.toLocaleTimeString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: true,
+    });
+    time = time.replace("AM", "").replace("PM", "");
+
+    let day = date.getDate();
+
+    const months = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+    ];
+    let month = months[date.getMonth()];
+
+    let weekdays = [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+    ];
+    let weekday = weekdays[date.getDay() - 1];
+
+    let formatDate = day + " " + month + " " + weekday;
+</script>
+
 <main>
-    <h1>Muse</h1>
+    <div class="timebx">
+        <div class="time">{time}</div>
+        <div class="date">{formatDate}</div>
+    </div>
 </main>
